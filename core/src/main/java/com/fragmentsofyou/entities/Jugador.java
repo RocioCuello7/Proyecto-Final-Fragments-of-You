2696af82068bd2a8c99929c073b5eadc7a4ca2ad
ValentinoDacal <valentinodacal2@gmail.com>
2026-08-13 22:47:43 -0300

    Implementar clase Jugador, desacoplar entrada de teclado/mouse y optimizar colisiones

---
+++ core/src/main/java/com/fragmentsofyou/entities/Jugador.java
@@ -0,0 +1,80 @@
+package com.fragmentsofyou.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.fragmentsofyou.handlers.MapCollision;
+
+public class Jugador{
+
+    private Texture texture;
+    private float x, y;
+    private float speed = 90f;
+    private float rotacion;
+
+    private float dirX, dirY;
+    private float width = 10f;
+    private float height = 10f;
+
+    private Vector3 mousePos = new Vector3();
+
+    public Jugador(float startX, float startY) {
+        this.x = startX;
+        this.y = startY;
+        this.texture = new Texture("meiSprite.png");
+    }
+
+    public void handleInput(Viewport viewport) {
+        mousePos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+        viewport.unproject(mousePos);
+
+        float deltaX = mousePos.x - x;
+        float deltaY = mousePos.y - y;
+        rotacion = MathUtils.atan2(deltaY, deltaX) * MathUtils.radiansToDegrees;
+
+        dirX = 0;
+        dirY = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) dirY += 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) dirY -= 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) dirX += 1;
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) dirX -= 1;
+    }
+
+    public void update(float dt, MapCollision mapCollision) {
+        if (dirY != 0) {
+            float intentoY = y + (dirY * speed * dt);
+            if (!mapCollision.isColliding(x, intentoY, width, height)) {
+                y = intentoY;
+            }
+        }
+
+        if (dirX != 0) {
+            float intentoX = x + (dirX * speed * dt);
+            if (!mapCollision.isColliding(intentoX, y, width, height)) {
+                x = intentoX;
+            }
+        }
+    }
+
+    public void render(SpriteBatch sb) {
+        sb.draw(texture,
+            x - 5, y - 5,
+            5, 5,
+            10, 10,
+            1, 1,
+            rotacion,
+            0, 0, 32, 32, false, false);
+    }
+
+    public float getX() { return x; }
+    public float getY() { return y; }
+
+    public void dispose() {
+        if (texture != null) texture.dispose();
+    }
+}
--- core/src/main/java/com/fragmentsofyou/states/Play.java
+++ core/src/main/java/com/fragmentsofyou/states/Play.java
@@ -1,10 +1,11 @@
-
 package com.fragmentsofyou.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -12,10 +13,9 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.fragmentsofyou.entities.Jugador;
 import com.fragmentsofyou.handlers.GameStateManager;
 import com.fragmentsofyou.handlers.MapCollision;
-import com.badlogic.gdx.maps.MapLayer;
-import com.badlogic.gdx.maps.MapObject;
 
 public class Play extends GameState {
 
@@ -25,16 +25,7 @@ public class Play extends GameState {
     private MapCollision mapCollision;
     private OrthogonalTiledMapRenderer mapRenderer;
 
-    private Texture meiSprite;
-    private float playerX;
-    private float playerY;
-    private float speed = 220f;
-    private float rotacionJugador;
-
-    private float playerWidth = 16f;   // Ancho de la caja de colisión
-    private float playerHeight = 16f;  // Alto de la caja de colisión
-
-    private Vector3 mousePos = new Vector3();
+    private Jugador jugador;
 
     public Play(GameStateManager gsm) {
         super(gsm);
@@ -46,85 +37,47 @@ public class Play extends GameState {
         mapCollision = new MapCollision(map, "paredes y muebles");
         mapRenderer = new OrthogonalTiledMapRenderer(map);
 
-        meiSprite = new Texture("meiSprite.png");
+        float spawnX = 240 / 2f;
+        float spawnY = 192 / 2f;
 
         MapLayer capa = map.getLayers().get("paredes y muebles");
         if (capa != null && capa.getObjects().get("spawn") != null) {
             MapObject spawnPoint = capa.getObjects().get("spawn");
-            playerX = (float) spawnPoint.getProperties().get("x");
-            playerY = (float) spawnPoint.getProperties().get("y");
-        } else {
-            playerX = 240 / 2f;
-            playerY = 192 / 2f;
-
+            spawnX = (float) spawnPoint.getProperties().get("x");
+            spawnY = (float) spawnPoint.getProperties().get("y");
         }
+
+        jugador = new Jugador(spawnX,spawnY);
     }
 
     @Override
     public void handleInput() {
-        // Vacío para evitar doble movimiento
+        jugador.handleInput(playView);
     }
 
     @Override
     public void update(float dt) {
         handleInput();
 
-        mousePos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-        playView.unproject(mousePos);
+        jugador.update(dt,mapCollision);
 
-        float nuevaX = playerX;
-        float nuevaY = playerY;
-
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            nuevaY += speed * dt;
-            if (!mapCollision.isColliding(nuevaX, nuevaY, playerWidth, playerHeight)) {
-                playerY = nuevaY;
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            nuevaY -= speed * dt;
-            if (!mapCollision.isColliding(nuevaX, nuevaY, playerWidth, playerHeight)) {
-                playerY = nuevaY;
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            nuevaX -= speed * dt;
-            if (!mapCollision.isColliding(nuevaX, nuevaY, playerWidth, playerHeight)) {
-                playerX = nuevaX;
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            nuevaX += speed * dt;
-            if (!mapCollision.isColliding(nuevaX, nuevaY, playerWidth, playerHeight)) {
-                playerX = nuevaX;
-            }
-        }
-
-        cam.position.set(playerX, playerY, 0);
+        cam.position.set(jugador.getX(), jugador.getY(), 0);
         cam.update();
     }
 
     @Override
     public void render() {
-        playView.apply();
-
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        playView.apply();
 
         mapRenderer.setView(cam);
         mapRenderer.render();
 
         sb.setProjectionMatrix(cam.combined);
-
         sb.begin();
-        // Dibujamos manteniendo el tamaño real de 32x32 de tu textura centrado en playerX/Y
-        sb.draw(meiSprite,
-            playerX - 5, playerY - 5,
-            16, 16,
-            10, 10,
-            1, 1,
-            rotacionJugador,
-            0, 0, 32, 32, false, false);
+        jugador.render(sb);
         sb.end();
     }
 
@@ -137,6 +90,6 @@ public class Play extends GameState {
     public void dispose() {
         if (map != null) map.dispose();
         if (mapRenderer != null) mapRenderer.dispose();
-        if (meiSprite != null) meiSprite.dispose();
+        if (jugador != null) jugador.dispose();
     }
 }
